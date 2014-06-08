26a0fc48973b3e3789a8920ea76cdedb8e1613ae
Yannick Francois <yannick.francois@free.fr>
2014-06-08 15:42:15 +0200

    Use a env var to connect to REDIS

--- tweet_search.rb
+++ tweet_search.rb
@@ -8,13 +8,15 @@ client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 counter = 0
 client.filter(:track => "operobespierre", :lang => "fr") do |object|
   if object.is_a?(Twitter::Tweet)
     counter += 1
-    tweet = Redis.new
-    tweet.set "robonova:tweet:#{counter}", {author_name: object.user[:name], status: object.text, profile_image_url: object.user.profile_image_url.to_s}.to_json
-    tweet.save
+    REDIS.set "robonova:tweet:#{counter}", {author_name: object.user[:name], status: object.text, profile_image_url: object.user.profile_image_url.to_s}.to_json
+    REDIS.save
   end
   if counter >= ENV['TWEET_LIMIT'].to_i
     counter = 0
